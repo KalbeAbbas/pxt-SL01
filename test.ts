2908abd5b7270155d1b32ee9f3f59877fa5052bb
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-12-12 12:54:27 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,2 @@
 basic.forever(function () {
-    basic.showNumber(SL01.getLUX(Illuminance.Lux))
-    basic.showNumber(SL01.getLUX(Illuminance.FootCandle))
-    basic.showNumber(SL01.getUVA())
-    basic.showNumber(SL01.getUVB())
-    basic.showNumber(SL01.getUVIndex())
-    basic.pause(100)
 })
